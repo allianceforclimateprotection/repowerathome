9c9f01ec6129d3b1a7bf2a4a24ef363b3f6a711d
Eric Buckley <eric.buckley@gmail.com>
2009-12-09 10:04:47 -0500

    seperated the rah* url patterns for better readability

--- urls.py
+++ urls.py
@@ -7,18 +7,23 @@ from django.core.urlresolvers import reverse
 from django.contrib import admin
 admin.autodiscover()
 
+# OPTIMIZE: we can wrap the regex patterns in the url function to insure there are no reverse conflicts
 urlpatterns = patterns('',
-    (r'^$', 'www.rah.views.index'),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^login/$', 'django.contrib.auth.views.login'),
-    (r'^register/$', 'www.rah.views.register'),
     (r'^logout/$', 'django.contrib.auth.views.logout', {'next_page':'/'}),
-    (r'^actions/$', 'www.rah.views.action_browse'),
-    (r'^actions/([a-z0-9-]+)/$', 'www.rah.views.action_cat'),
-    (r'^actions/([a-z0-9-]+)/([a-z0-9-]+)/$', 'www.rah.views.action_detail'),
-    
+
     (r'^admin/', include(admin.site.urls)),
     #(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_DOC_ROOT}),
-    (r'^(?P<username>\w+)/edit/$', 'www.rah.views.profile_edit'),
-    (r'^(?P<username>\w+)/$', 'www.rah.views.profile'),
+)
+
+urlpatterns += patterns('www.rah.views',
+    (r'^$', 'index'),
+    # OPTIMIZE: we can remove our custom register view altogether and just specify our custom from as a parameter in the url pattern
+    (r'^register/$', 'register'),
+    (r'^actions/$', 'action_browse'),
+    (r'^actions/([a-z0-9-]+)/$', 'action_cat'),
+    (r'^actions/([a-z0-9-]+)/([a-z0-9-]+)/$', 'action_detail'),
+    (r'^(?P<username>\w+)/edit/$', 'profile_edit'),
+    (r'^(?P<username>\w+)/$', 'profile'),
 )
