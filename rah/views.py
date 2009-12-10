0e7a961e5973717c5c92fac77a77c9d33a2eed12
Jon Lesser <jon.lesser@climateprotect.org>
2009-12-09 22:28:29 -0500

    Adding lists of recommended actions, earned points, and actions with completed tasks to logged in home page Handle saving of completed action tasks Completes tickets #8 #11 #12 #30

--- rah/views.py
+++ rah/views.py
@@ -16,16 +16,21 @@ def index(request):
     """
     # If the user is logged in, show them the logged in homepage and bail
     if request.user.is_authenticated():
-        # TODO Get a list of relevant actions
+        # Get a list of relevant actions
+        action_recs = Action.objects.select_related().all()[:5]
 
-        # Get a list of completed actions tasks
-
+        # Get a list of actions in progress
+        in_progress = UserActionTask.objects.select_related().filter(user=request.user)
+        
         # Get a list of points earned by this user and their total points
-        points = Points.objects.filter(user=user)[:10]
-        total_points = Points.objects.filter(user=user).aggregate(Sum('points'))['points__sum']
+        points = Points.objects.filter(user=request.user)[:10]
+        total_points = Points.objects.filter(user=request.user).aggregate(Sum('points'))['points__sum']
         
         return render_to_response('rah/home_logged_in.html', {
-            'points': points
+            'points': points,
+            'total_points': total_points,
+            'in_progress': in_progress,
+            'action_recs': action_recs,
         }, context_instance=RequestContext(request))
     
     # Setup and handle email form on logged out home page
@@ -77,11 +82,35 @@ def action_detail(request, catSlug, actionSlug):
     """Detail page for an action"""
     # Lookup the action
     action = get_object_or_404(Action, slug=actionSlug)
-    tasks = UserActionTask.get_tasks_by_user_action_with_default(action, request.user)
-
+    
+    #  Handle the POST if a task is being completed and task_id is an integer
+    if request.method == 'POST' and request.POST.get('task_id').isdigit():
+        try:
+            # Look for a row in UserActionTask table and delete it if task_completed is unchecked
+            user_action_task = UserActionTask.objects.get(user=request.user, action_task=request.POST.get('task_id'))
+            if request.POST.get('task_completed') == None:
+                user_action_task.delete()
+        except:
+            # There was no row in UserActionTask, so create one if task_completed is checked
+            if request.POST.get('task_completed') == 'on':
+                UserActionTask(user=request.user, action_task=ActionTask(pk=request.POST.get('task_id'))).save()
+    
+    # Get action tasks conditionally depending on whether or not they're logged in
+    if(request.user.is_authenticated()):
+        # If they're logged in, grab the completed time for any completed tasks
+        action_tasks = ActionTask.objects.filter(action=action.id).extra(
+            select_params = (request.user.id,), 
+            select = { 'completed': 'SELECT rah_useractiontask.completed \
+                                     FROM rah_useractiontask \
+                                     WHERE rah_useractiontask.user_id = %s AND \
+                                     rah_useractiontask.action_task_id = rah_actiontask.id' }
+        )
+    else:
+        action_tasks = ActionTask.objects.filter(action=action.id)
+    
     return render_to_response('rah/action_detail.html', {
                                 'action': action,
-                                'tasks': tasks
+                                'action_tasks': action_tasks
                               }, context_instance=RequestContext(request))
 
 def profile(request, username):
