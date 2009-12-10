fb7807e29677be4ba1fe9bc6ab7765e6ead55885
Jon Lesser <jon.lesser@climateprotect.org>
2009-12-09 22:23:32 -0500

    Changed task column name to action_task Dropped get_tasks_by_user_action_with_default function in favor of .extra() technique in views.action_detail

--- rah/models.py
+++ rah/models.py
@@ -45,31 +45,12 @@ class UserActionTask(models.Model):
     """
     class representing the ActionTasks a specific user has completed
     """
-    task = models.ForeignKey(ActionTask)
+    action_task = models.ForeignKey(ActionTask)
     user = models.ForeignKey(User)
-    completed = models.DateTimeField(blank=True, default=datetime.now)
+    completed = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
-        return u'%s completed at %s' % (self.task, self.completed)
-
-    @classmethod
-    def get_tasks_by_user_action_with_default(clas, action, user):
-        """docstring for get_tasks_by_user_action_with_default"""
-        from django.db import connection, transaction
-        cursor = connection.cursor()
-        query = """
-            SELECT at.name, at.content, at.sequence, uat.completed
-            FROM rah_useractiontask uat
-            RIGHT JOIN rah_actiontask at ON uat.task_id = at.id AND uat.user_id = %s
-            WHERE at.action_id = %s
-        """
-        cursor.execute(query, [user.id, action.id])
-        rows = cursor.fetchall()
-        user_action_tasks = []
-        for row in rows:
-            task = ActionTask(action=action, name=row[0], content=row[1], sequence=row[2])
-            user_action_tasks.append(UserActionTask(task=task, user=user, completed=row[3]))
-        return user_action_tasks
+        return u'%s completed at %s' % (self.action_task, self.completed)
 
 class Location(models.Model):
     name = models.CharField(max_length=200)
@@ -97,12 +78,11 @@ class Points(models.Model):
     
     REASONS = (
         (1, "Because we like you"),
-        (2, "Because we don't you"),
+        (2, "Because we don't like you"),
     )
     
     user = models.ForeignKey(User)
     points = models.IntegerField()
-    # TODO Change this to ActionTasks when that model is ready
     task = models.ForeignKey(ActionTask, related_name="task", null=True)
     reason = models.IntegerField(choices=REASONS, default='')
     created = models.DateTimeField(auto_now_add=True)
