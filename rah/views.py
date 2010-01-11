51fcf926cd0c40007e172e087132432f62461859
Jon Lesser <jon.lesser@climateprotect.org>
2010-01-11 10:11:33 -0500

    Moving profile nugget to home page (image, name, etc.)

--- rah/views.py
+++ rah/views.py
@@ -27,7 +27,8 @@ def index(request):
             'completed': completed,
             'recommended': recommended[:6], # Hack to only show 6 "recommended" actions
             'house_party_form': HousePartyForm(),
-            'chart_data': request.user.get_chart_data()
+            'chart_data': request.user.get_chart_data(),
+            'profile': request.user.get_profile(),
         }, context_instance=RequestContext(request))
     
     # Setup and handle email form on logged out home page
