cd8763578e5dc60033b0c01edcee2100f6bc717f
Jon Lesser <jon.lesser@climateprotect.org>
2010-01-12 10:19:19 -0500

    Setting better defaults for chart axis Adding media_url to js env Adding demo chart for when people have no data to chart

--- static/js/rah.js
+++ static/js/rah.js
@@ -152,36 +152,38 @@ var rah = {
             rah.mod_action_nugget.init();
             rah.mod_house_party.init();
             
-            var plot = $.plot($("#chart"),
-                   [ { data: chart_data['point_data'] }], {
-                       series: {
-                           lines: { show: true },
-                           points: { show: true, radius: 10 },
-                           shadowSize: 5,
-                       },
-                       grid: { hoverable: true, clickable: true, backgroundColor: { colors: ["#DDD", "#FFF"] } },
-                       legend: {show: false},
-                       xaxis: {mode: "time", autoscaleMargin: 0.1},
-                       yaxis: {min: 0},
-                     });
-
-            function showTooltip(x, y, index) {
-                $('<div class="chart_tooltip">' + chart_data["tooltips"][index] + '</div>').css( {
-                    position: 'absolute',
-                    display: 'none',
-                    top: y - 18,
-                    left: x + 5 ,
-                }).appendTo("body").fadeIn(300);
+            if (!chart_data['point_data'].length) {
+                $("#chart").html('<img src="' + media_url + 'images/theme/chart_demo.png" alt="sample chart"/>');
+            } else {
+                var plot = $.plot($("#chart"),
+                    [ { data: chart_data['point_data'] }], {
+                        series: {
+                            lines: { show: true },
+                            points: { show: true, radius: 10 },
+                            shadowSize: 5,
+                        },
+                        grid: { hoverable: true, clickable: true, backgroundColor: { colors: ["#DDD", "#FFF"] } },
+                        legend: {show: false},
+                        xaxis: {mode: "time", autoscaleMargin: 0.1, minTickSize: [1, "day"]},
+                        yaxis: {min: 0, tickDecimals: 0},
+                });
+                $("#chart").bind("plothover", function (event, pos, item) {
+                    if (item) {
+                        showTooltip(item.pageX, item.pageY, item.dataIndex);
+                    }
+                    else {
+                        $(".chart_tooltip").remove();
+                    }
+                });
+                function showTooltip(x, y, index) {
+                    $('<div class="chart_tooltip">' + chart_data["tooltips"][index] + '</div>').css( {
+                        position: 'absolute',
+                        display: 'none',
+                        top: y - 18,
+                        left: x + 5 ,
+                    }).appendTo("body").fadeIn(300);
+                }
             }
-
-            $("#chart").bind("plothover", function (event, pos, item) {
-                if (item) {
-                    showTooltip(item.pageX, item.pageY, item.dataIndex);
-                }
-                else {
-                    $(".chart_tooltip").remove();
-                }
-            });            
         },
     },
     
