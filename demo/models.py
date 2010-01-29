eb6cdbe0d00c34c230e037b37a03fc307f4e56f0
Eric Buckley <eric.buckley@gmail.com>
2010-01-29 13:20:03 -0500

    made vast improvements to the demo users_with_completes method my de normalizing the in_progress and completed counts in the action table

--- demo/models.py
+++ demo/models.py
@@ -59,9 +59,13 @@ class UserManager(models.Manager):
         """create x number of users with incremental names"""
         id = self.all().aggregate(models.Max("id"))["id__max"]
         next_id = id + 1 if id else 1
+        user_ids = []
         for id in range(number):
             name = "user_%s" % (id+next_id)
-            User(name=name).save()
+            user = User(name=name)
+            user.save()
+            user_ids.append(user.id)
+        return user_ids
         
 class User(DefaultModel):
     groups = models.ManyToManyField(Group)
@@ -105,6 +109,7 @@ class User(DefaultModel):
                                      demo_record.activity_id = demo_activity.id' })
                                      
         actiontask_dict = dict([(at.id, at) for at in action_tasks])
+        print actiontask_dict
         for action in actions:
             total_completes = 0
             for actiontask in action.actiontask_set.all():
@@ -129,6 +134,8 @@ class User(DefaultModel):
 class Action(DefaultModel):
     total_tasks = models.IntegerField(default=0)
     total_points = models.IntegerField(default=0)
+    users_in_progress = models.IntegerField(default=0)
+    users_completed = models.IntegerField(default=0)
     user_progress = models.ManyToManyField(User, through="UserActionProgress")
     
     def users_with_completes(self):
@@ -149,20 +156,35 @@ class Action(DefaultModel):
         return (users, in_progress, completed)
         
     def users_with_completes_denorm(self):
-        """
-        return a 3-tuple query set of user object lists, such that each user object has an additional attribute, completes,
-        which identifies how many tasks the user has completed for the given action.
+        in_progress_count = UserActionProgress.objects.filter(action=self, completed=0).count()
+        completed_count = UserActionProgress.objects.filter(action=self, completed=1).count()
 
-        Note: only users that have began completeing task for the action will be listed
+        in_progress = UserActionProgress.objects.select_related().filter(action=self, completed=0)[:5]
+        completed = UserActionProgress.objects.select_related().filter(action=self, completed=1)[:5]
 
-        The first list in the tuple is for all users that have made progress on the action, the second list is for all users
-        that are working on the action (in progress) and the final list is for all users that have completed the action
-        """
-        users = User.objects.filter(useractionprogress__action=self).extra(select={"completes": "completes"})
-        in_progress = [user for user in users if self.total_tasks > user.completes and user.completes > 0]
-        completed = [user for user in users if self.total_tasks == user.completes]
+        return (in_progress_count, [uap.user for uap in in_progress], completed_count, [uap.user for uap in completed])
+        
+    def users_with_completes_denorm_raw(self):
+        from django.db import connection, transaction
+        cursor = connection.cursor()
 
-        return (users, in_progress, completed)
+        count_query = "SELECT COUNT(CASE WHEN completed = 0 THEN 1 END), COUNT(CASE WHEN completed = 1 THEN 1 END) FROM demo_useractionprogress WHERE action_id = %s" % self.id
+
+        cursor.execute(count_query)
+        in_progress_count, completed_count = cursor.fetchone()
+
+        in_progress_where = "WHERE duap.action_id = %s AND duap.completed = 0" % self.id
+        completed_where = "WHERE duap.action_id = %s AND duap.completed = 1" % self.id
+        in_progress = list(User.objects.raw("SELECT * FROM demo_user du JOIN demo_useractionprogress duap ON du.id = duap.user_id %s LIMIT 5" % (in_progress_where)))
+        completed = list(User.objects.raw("SELECT * FROM demo_user du JOIN demo_useractionprogress duap ON du.id = duap.user_id %s  LIMIT 5" % (completed_where)))
+
+        return (in_progress_count, in_progress, completed_count, completed)
+        
+    def users_with_completes_denorm_best(self):
+        in_progress = UserActionProgress.objects.select_related().filter(action=self, completed=0)[:5]
+        completed = UserActionProgress.objects.select_related().filter(action=self, completed=1)[:5]
+
+        return (self.users_in_progress, [uap.user for uap in in_progress], self.users_completed, [uap.user for uap in completed])
     
 class Activity(DefaultModel):
     points = models.IntegerField(blank=True, null=True)
@@ -172,9 +194,9 @@ class ActionTask(Activity):
     action = models.ForeignKey(Action)
     
 class RecordManager(models.Manager):
-    def create_random_records(self, mean, sigma=3.5):
+    def create_random_records(self, mean, sigma=3.5, users=None):
         import math, random
-        users = [user["id"] for user in User.objects.all().values("id")]
+        users = users if users != None else [user["id"] for user in User.objects.all().values("id")]
         activities = Activity.objects.all()
         activities_list = [act.id for act in activities]
         activities_dict = dict([(act.id, act) for act in activities])
@@ -223,6 +245,7 @@ class UserActionProgress(models.Model):
         user = models.ForeignKey(User)
         action = models.ForeignKey(Action)
         completes = models.IntegerField(default=0)
+        completed = models.IntegerField(default=0)
         
 class ChartPoint(object):
     """docstring for ChartPoint"""
@@ -259,11 +282,26 @@ def record_removed(sender, **kwargs):
     record_table_changed(sender, increase=False, **kwargs)
 
 def record_table_changed(sender, instance, increase, **kwargs):
+    # TODO: this entire function needs to be wrapped in a transaction
     try:
-        obj, created = UserActionProgress.objects.get_or_create(user=instance.user, action=instance.activity.actiontask.action)
-        obj.completes += 1 if increase else -1
+        action = instance.activity.actiontask.action
+        obj, created = UserActionProgress.objects.get_or_create(user=instance.user, action=action)
+        new_completes = obj.completes + 1 if increase else  obj.completes - 1
+        new_completed = 1 if new_completes >= action.total_tasks else 0
+        inprogress = 1  if obj.completed != 1 and obj.completes > 0 else 0 # the useraction is currently in_progress if it is not completed, but the completes count is greater than 0
+        new_inprogress = 1 if new_completed != 1 and new_completes > 0 else 0 # the useraction is going to be in_progress if it will not be completed, but the new completes count is greater than 0
+        
+        if obj.completed != new_completed:
+            action.users_completed += new_completed - obj.completed
+        if inprogress != new_inprogress:
+            action.users_in_progress += new_inprogress - inprogress
+        if obj.completed != new_completed or inprogress != new_inprogress:
+            action.save()
+        
+        obj.completes = new_completes
+        obj.completed = new_completed
         obj.save()
-    except Exception:
+    except ActionTask.DoesNotExist:
         pass
         
 models.signals.post_save.connect(record_added, sender=Record)
--- demo_tester.py
+++ demo_tester.py
@@ -11,19 +11,24 @@ from demo.utils import *
 print "This program is used to test the query speeds of the demo app given different sizes of Users, Groups and Records."
 print "For accurate results make sure to delete all demo.* tables in your database first and rerun the syncdb command."
 
-num_of_users = int(raw_input("How many users would you like to create? "))
-print "Creating users..."
-User.objects.create_test_users(num_of_users)
+user_ids = None
+create_more_users = raw_input("Would you like to create new users ([y]|n)? ")
+if not create_more_users.startswith("n"):
+    num_of_users = int(raw_input("How many users would you like to create? "))
+    print "Creating users..."
+    user_ids = User.objects.create_test_users(num_of_users)
 
 size_of_group = int(raw_input("How many users would you like to put in your new group? "))
 print "Creating group..."
 group = Group.objects.create_test_group(size_of_group)
 
-print "We will randomly create records for each user based on a Gaussian distribution."
-records_mean = int(raw_input("\tWhat would you like to set the mean to? "))
-records_sigma = float(raw_input("\tAnd the sigma value? "))
-print "Creating records (this could take a while)..."
-Record.objects.create_random_records(records_mean, records_sigma)
+create_more_records = raw_input("Would you like to create new records ([y]|n)? ")
+if not create_more_records.startswith("n"):
+    print "We will randomly create records for each user based on a Gaussian distribution."
+    records_mean = int(raw_input("\tWhat would you like to set the mean to? "))
+    records_sigma = float(raw_input("\tAnd the sigma value? "))
+    print "Creating records (this could take a while)..."
+    Record.objects.create_random_records(records_mean, records_sigma, user_ids)
 
 print "Running Group tests..."
 timeit(group.how_many_members)()
@@ -31,7 +36,7 @@ timeit(group.completed_actions_by_user)()
 timeit(group.completed_actions_by_user_denorm)()
 timeit(group.members_with_points)()
 timeit(group.get_latest_records)()
-
+# 
 users = User.objects.all()
 user = users[random.randint(0, len(users) - 1)]
 print "Running User tests..."
@@ -45,3 +50,5 @@ action = actions[random.randint(0, len(actions) - 1)]
 print "Running Action tests..."
 timeit(action.users_with_completes)()
 timeit(action.users_with_completes_denorm)()
+timeit(action.users_with_completes_denorm_raw)()
+timeit(action.users_with_completes_denorm_best)()
