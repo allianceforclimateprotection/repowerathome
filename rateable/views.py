0b643e058c8c0819b3a1a8eae1530e5e62504178
Eric Buckley <eric.buckley@gmail.com>
2010-02-18 12:44:55 -0500

    marking comments as helpful will now be invoked via ajax

--- rateable/views.py
+++ rateable/views.py
@@ -1,8 +1,11 @@
+import pdb
+
 from django.contrib.auth.decorators import login_required
 from django.contrib import contenttypes, messages
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
+from django.http import HttpResponse
 from django.shortcuts import redirect, render_to_response
 from django.template import RequestContext
 from django.views.decorators.http import require_POST
@@ -31,11 +34,23 @@ def rate(request, rating_widget=IsHelpfulWidget, next=None, using=None, success_
     score = rating_widget.determine_score(request.POST)
     rating, created = Rating.objects.create_or_update(content_type=content_type, object_pk=object_pk, 
         user=request.user, score=score)
+    
     if success_message and rating:
         messages.success(request, success_message)
     elif error_message and not rating:
         messages.error(request, error_message)
+    # pdb.set_trace()
     next = request.POST.get("next", next)
     if next:
         return redirect(next)
-    return render_to_response("rateable/rated.html", {"rating":rating}, context_instance=RequestContext(request))    
+        
+    template_list = [
+        "rateable/%s/rated.html" % content_type.model_class()._meta.app_label,
+        "rateable/rated.html",
+    ]
+    if request.is_ajax():
+        template_list = [ 
+            "rateable/%s/ajax/rated.html" % content_type.model_class()._meta.app_label,
+            "rateable/ajax/rated.html",
+        ] + template_list 
+    return render_to_response(template_list, {"rating":rating}, context_instance=RequestContext(request))    
