e7bbf40ad23b0db2390471bd46347f0938f596f4
Eric Buckley <eric.buckley@gmail.com>
2010-03-10 10:49:31 -0500

    added exception handling if rate is invoked with bad parameters

--- rateable/views.py
+++ rateable/views.py
@@ -1,11 +1,9 @@
-import pdb
-
 from django.contrib.auth.decorators import login_required
 from django.contrib import contenttypes, messages
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import redirect, render_to_response
 from django.template import RequestContext
 from django.views.decorators.http import require_POST
@@ -25,11 +23,9 @@ def rate(request, rating_widget=IsHelpfulWidget, next=None, using=None, success_
         content_type = ContentType.objects.get(pk=content_type_pk)
         target = content_type.get_object_for_this_type(pk=object_pk)
     except AttributeError:
-        # TODO: handle error if content type is invalid
-        pass
+        raise Http404("No type found matching %s" % content_type_pk)
     except ObjectDoesNotExist:
-        # TODO: handle error if object can not be found
-        pass
+        raise Http404("No object found matching %s" % object_pk)
     # if the score is defined in the post data override that provided
     score = rating_widget.determine_score(request.POST)
     rating, created = Rating.objects.create_or_update(content_type=content_type, object_pk=object_pk, 
@@ -39,7 +35,6 @@ def rate(request, rating_widget=IsHelpfulWidget, next=None, using=None, success_
         messages.success(request, success_message)
     elif error_message and not rating:
         messages.error(request, error_message)
-    # pdb.set_trace()
     next = request.POST.get("next", next)
     if next:
         return redirect(next)
