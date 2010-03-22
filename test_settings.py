dffbca541e369734315f4e99ecdbf92e8a110311
Jon Lesser <jon.lesser@climateprotect.org>
2010-03-22 14:53:35 -0400

    Checking in test_settings.py

---
+++ test_settings.py
@@ -0,0 +1,37 @@
+try:
+    from settings import *
+except ImportError:
+    print 'settings could not be imported'
+    pass
+    
+DATABASE_ENGINE   = 'sqlite3'  # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+# DATABASE_NAME     = ':memory:' # Or path to database file if using sqlite3.
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.comments',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'rah',
+    'rateable',
+    'rateable.tests',
+    'records',
+    'geo',
+    'invite',
+    'basic.blog',
+    'basic.inlines',
+    'tagging',
+    'twitter_app',
+    'search_widget',
+    'groups',
+    'sorl.thumbnail',
+    'flagged',
+    'django_extensions',
+    'debug_toolbar',
+)
+
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
