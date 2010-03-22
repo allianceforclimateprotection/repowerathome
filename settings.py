a446c3139dddb926d4aa8ecfec5be8500b41a3fe
Jon Lesser <jon.lesser@climateprotect.org>
2010-03-22 14:51:40 -0400

    removing template caching from settings (added to local settings on the server)

--- settings.py
+++ settings.py
@@ -37,10 +37,8 @@ ADMIN_MEDIA_PREFIX = '/media/'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    (
-        'django.template.loaders.cached.Loader',
-        ('django.template.loaders.filesystem.Loader', 'django.template.loaders.app_directories.Loader',)
-    ),
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
 )
 
 MIDDLEWARE_CLASSES = (
