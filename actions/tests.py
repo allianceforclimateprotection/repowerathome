2693a78fdc9c66790e105962bc6c55020ad7b3d5
Eric Buckley <eric.buckley@gmail.com>
2010-04-09 15:25:34 -0400

    created unit tests for actions app [completed: 205]

--- actions/tests.py
+++ actions/tests.py
@@ -33,9 +33,14 @@ class ActionTest(TestCase):
     def test_complete_for_user(self):
         self.failUnlessRaises(UserActionProgress.DoesNotExist, UserActionProgress.objects.get,
             user=self.user, action=self.iwh)
+        self.failUnlessEqual(self.iwh.users_completed, 0)
+        
         self.iwh.complete_for_user(self.user)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        
         uap = UserActionProgress.objects.get(user=self.user, action=self.iwh)
         self.failUnlessEqual(uap.is_completed, True)
+        self.failUnlessEqual(self.iwh.users_completed, 1)
         records = Record.objects.filter(user=self.user, activity__slug="action_complete")
         self.failUnlessEqual(len(records), 1)
         record = records[0]
@@ -45,10 +50,142 @@ class ActionTest(TestCase):
         
     def test_undo_for_user(self):
         self.test_complete_for_user()
+        self.failUnlessEqual(self.iwh.users_completed, 1)
         
         self.iwh.undo_for_user(self.user)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        
         uap = UserActionProgress.objects.get(user=self.user, action=self.iwh)
         self.failUnlessEqual(uap.is_completed, False)
+        self.failUnlessEqual(self.iwh.users_completed, 0)
         record = Record.objects.filter(user=self.user, activity__slug="action_complete")
         self.failUnlessEqual(list(record), [])
+        
+    def test_commit_for_user(self):
+        self.failUnlessRaises(UserActionProgress.DoesNotExist, UserActionProgress.objects.get,
+            user=self.user, action=self.iwh)
+        self.failUnlessEqual(self.iwh.users_committed, 0)
+        today = datetime.date.today()
+        
+        self.iwh.commit_for_user(self.user, today)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        
+        uap = UserActionProgress.objects.get(user=self.user, action=self.iwh)
+        self.failUnlessEqual(uap.date_committed, today)
+        self.failUnlessEqual(self.iwh.users_committed, 1)
+        records = Record.objects.filter(user=self.user, activity__slug="action_commitment")
+        self.failUnlessEqual(len(records), 1)
+        record = records[0]
+        self.failUnlessEqual(record.void, False)
+        rao = record.content_objects.all()[0]
+        self.failUnlessEqual(rao.content_object, self.iwh)
+        
+    def test_cancel_for_user(self):
+        self.test_commit_for_user()
+        self.failUnlessEqual(self.iwh.users_committed, 1)
+        
+        self.iwh.cancel_for_user(self.user)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        
+        uap = UserActionProgress.objects.get(user=self.user, action=self.iwh)
+        self.failUnlessEqual(uap.date_committed, None)
+        self.failUnlessEqual(self.iwh.users_committed, 0)
+        record = Record.objects.filter(user=self.user, activity__slug="action_commitment")
+        self.failUnlessEqual(list(record), [])
+        
+    def test_action_completed_aggregate_signal(self):
+        user_2 = User.objects.create_user(username="test_2", password="test", email="test_2@test.com")
+        self.failUnlessEqual(self.iwh.users_completed, 0)
+        self.failUnlessEqual(self.csp.users_completed, 0)
+        
+        self.iwh.complete_for_user(self.user)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        self.csp = Action.objects.get(slug="cut-standby-power")
+        self.failUnlessEqual(self.iwh.users_completed, 1)
+        self.failUnlessEqual(self.csp.users_completed, 0)
+        
+        self.csp.complete_for_user(user_2)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        self.csp = Action.objects.get(slug="cut-standby-power")
+        self.failUnlessEqual(self.iwh.users_completed, 1)
+        self.failUnlessEqual(self.csp.users_completed, 1)
+        
+        self.iwh.complete_for_user(user_2)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        self.csp = Action.objects.get(slug="cut-standby-power")
+        self.failUnlessEqual(self.iwh.users_completed, 2)
+        self.failUnlessEqual(self.csp.users_completed, 1)
+        
+        self.csp.complete_for_user(self.user)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        self.csp = Action.objects.get(slug="cut-standby-power")
+        self.failUnlessEqual(self.iwh.users_completed, 2)
+        self.failUnlessEqual(self.csp.users_completed, 2)
+        
+        self.csp.complete_for_user(user_2)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        self.csp = Action.objects.get(slug="cut-standby-power")
+        self.failUnlessEqual(self.iwh.users_completed, 2)
+        self.failUnlessEqual(self.csp.users_completed, 2)
+        
+        self.csp.undo_for_user(user_2)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        self.csp = Action.objects.get(slug="cut-standby-power")
+        self.failUnlessEqual(self.iwh.users_completed, 2)
+        self.failUnlessEqual(self.csp.users_completed, 1)
+        
+        self.csp.undo_for_user(self.user)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        self.csp = Action.objects.get(slug="cut-standby-power")
+        self.failUnlessEqual(self.iwh.users_completed, 2)
+        self.failUnlessEqual(self.csp.users_completed, 0)
+        
+    def test_action_committed_aggregate_signal(self):
+        user_2 = User.objects.create_user(username="test_2", password="test", email="test_2@test.com")
+        today = datetime.date.today()
+        self.failUnlessEqual(self.iwh.users_committed, 0)
+        self.failUnlessEqual(self.csp.users_committed, 0)
+
+        self.iwh.commit_for_user(self.user, today)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        self.csp = Action.objects.get(slug="cut-standby-power")
+        self.failUnlessEqual(self.iwh.users_committed, 1)
+        self.failUnlessEqual(self.csp.users_committed, 0)
+
+        self.csp.commit_for_user(user_2, today)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        self.csp = Action.objects.get(slug="cut-standby-power")
+        self.failUnlessEqual(self.iwh.users_committed, 1)
+        self.failUnlessEqual(self.csp.users_committed, 1)
+
+        self.iwh.commit_for_user(user_2, today)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        self.csp = Action.objects.get(slug="cut-standby-power")
+        self.failUnlessEqual(self.iwh.users_committed, 2)
+        self.failUnlessEqual(self.csp.users_committed, 1)
+
+        self.csp.commit_for_user(self.user, today)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        self.csp = Action.objects.get(slug="cut-standby-power")
+        self.failUnlessEqual(self.iwh.users_committed, 2)
+        self.failUnlessEqual(self.csp.users_committed, 2)
+
+        self.csp.commit_for_user(user_2, today)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        self.csp = Action.objects.get(slug="cut-standby-power")
+        self.failUnlessEqual(self.iwh.users_committed, 2)
+        self.failUnlessEqual(self.csp.users_committed, 2)
+        
+        self.csp.cancel_for_user(user_2)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        self.csp = Action.objects.get(slug="cut-standby-power")
+        self.failUnlessEqual(self.iwh.users_committed, 2)
+        self.failUnlessEqual(self.csp.users_committed, 1)
+        
+        self.csp.cancel_for_user(self.user)
+        self.iwh = Action.objects.get(slug="insulate-water-heater")
+        self.csp = Action.objects.get(slug="cut-standby-power")
+        self.failUnlessEqual(self.iwh.users_committed, 2)
+        self.failUnlessEqual(self.csp.users_committed, 0)
+        
         
