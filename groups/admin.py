710d4141193f55254e2734074ad632344e7a0143
Eric Buckley <eric.buckley@gmail.com>
2010-04-12 12:47:05 -0400

    updated admin interface for groups

--- groups/admin.py
+++ groups/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from models import Group, GroupUsers
 
-admin.site.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_geo_group",)
+    list_filter = ("is_geo_group",)
+    readonly_fields = ("is_geo_group", "location_type", "sample_location", "parent",)
+
+admin.site.register(Group, GroupAdmin)
 admin.site.register(GroupUsers)
