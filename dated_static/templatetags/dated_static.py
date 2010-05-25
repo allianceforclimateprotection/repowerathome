1191ccb6b5e9cfdceeae8b095be87fed13db8a6a
Jon Lesser <jon.lesser@climateprotect.org>
2010-05-25 10:55:00 -0400

    Styled blog Added two new action header images and 10 new action thumb images

--- dated_static/templatetags/dated_static.py
+++ dated_static/templatetags/dated_static.py
@@ -7,8 +7,10 @@ register = template.Library()
 
 @register.simple_tag
 def dated_static(static_file):
-    abs_path = os.path.join(settings.MEDIA_ROOT, static_file)
-    paths = os.path.split(static_file)
-    m_time = int(os.stat(abs_path).st_mtime)
-    return os.path.join(settings.MEDIA_URL, os.path.join(paths[0], "%s_%s" % (m_time, paths[1])))
-    
+    try:
+        abs_path = os.path.join(settings.MEDIA_ROOT, static_file)
+        paths = os.path.split(static_file)
+        m_time = int(os.stat(abs_path).st_mtime)
+        return os.path.join(settings.MEDIA_URL, os.path.join(paths[0], "%s_%s" % (m_time, paths[1])))
+    except:
+        return static_file
