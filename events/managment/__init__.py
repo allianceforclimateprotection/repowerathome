b73b67d84d54d8238c523771fb0b3c9124472a85
Eric Buckley <eric.buckley@gmail.com>
2010-06-04 10:01:18 -0400

    added default set of event challenges

--- events/managment/__init__.py
+++ events/managment/__init__.py
@@ -2,7 +2,7 @@ from django.db.models import get_models, signals
 from events import models as events_app
 
 def create_default_events(app, created_models, verbosity, **kwargs):
-    from events.models import EventType
+    from events.models import EventType, Challenge
     from django.core.management import call_command
     if EventType in created_models and kwargs.get('interactive', True):
         msg = "\nYou just installed the event_types table, would you like to install the " \
@@ -14,6 +14,17 @@ def create_default_events(app, created_models, verbosity, **kwargs):
                 continue
             if confirm == 'yes':
                 call_command("loaddata", "events/fixtures/event_types.json", verbosity=0, interactive=True)
+            break
+    if Challenge in created_models and kwargs.get('interactive', True):
+        msg = "\nYou just installed the challenges table, would you like to install the " \
+                "default set of challenges? (yes/no): "
+        confirm = raw_input(msg)
+        while 1:
+            if confirm not in ('yes', 'no'):
+                confirm = raw_input('Please enter either "yes" or "no": ')
+                continue
+            if confirm == 'yes':
+                call_command("loaddata", "events/fixtures/challenges.json", verbosity=0, interactive=True)
             break
 
 signals.post_syncdb.connect(create_default_events, sender=events_app, 
