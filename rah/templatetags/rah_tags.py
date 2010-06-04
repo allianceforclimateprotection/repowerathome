beadd272949ab48d0af9167264c4d288f86459c0
Jon Lesser <jon.lesser@climateprotect.org>
2010-06-04 15:34:50 -0400

    Moved activity_by_slug template tag to records app (fixed import error that was only happening on Jon's dev environment) Incrementing css images

--- rah/templatetags/rah_tags.py
+++ rah/templatetags/rah_tags.py
@@ -1,8 +1,6 @@
 from django import template
 from django.core.urlresolvers import reverse
 
-from records.models import Activity
-
 register = template.Library()
 
 class UserNode(template.Node):
@@ -53,34 +51,3 @@ def deslug(value):
 def jsonify(value):
     return value.replace("\n", " ").replace('"', '\\"')
 
-class ActivityNode(template.Node):
-    def __init__(self, activity, var_name):
-        self.activity = activity
-        self.var_name = var_name
-        
-    def render(self, context):
-        context[self.var_name] = self.activity
-        return ""
-
-@register.tag
-def activity_by_slug(parser, token):
-    """
-    Assign an activity object to a variable based on the slug provided.
-    
-    Syntax::
-        {% activity_by_slug [slug] as [variable_name] %}
-        
-    Example usage::
-        {% activity_by_slug group_create as group_create_activity %}
-    """
-    try:
-        tag_name, slug, as_token, var_name = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError, \
-            "%(tag_name)s tag requires 4 arguments {%% %(tag_name)s [slug] as [var_name] %%}" % {"tag_name": token.contents.split()[0]}
-            
-    if slug[0] == slug[-1] and slug[0] in ('"', "'"):
-        slug = slug[1:-1] #slug came through in quotes, strip them away
-    
-    activity = Activity.objects.get(slug=slug)
-    return ActivityNode(activity, var_name)
--- records/templatetags/record.py
+++ records/templatetags/record.py
@@ -1,5 +1,7 @@
 from django import template
 
+from ..models import Activity
+
 register = template.Library()
 
 class RecordNode(template.Node):
@@ -29,3 +31,35 @@ def render_record(parser, token):
     """
     record_expr = RecordNode.parse(parser, token)
     return RecordNode(record_expr)
+
+class ActivityNode(template.Node):
+    def __init__(self, activity, var_name):
+        self.activity = activity
+        self.var_name = var_name
+        
+    def render(self, context):
+        context[self.var_name] = self.activity
+        return ""
+
+@register.tag
+def activity_by_slug(parser, token):
+    """
+    Assign an activity object to a variable based on the slug provided.
+    
+    Syntax::
+        {% activity_by_slug [slug] as [variable_name] %}
+        
+    Example usage::
+        {% activity_by_slug group_create as group_create_activity %}
+    """
+    try:
+        tag_name, slug, as_token, var_name = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError, \
+            "%(tag_name)s tag requires 4 arguments {%% %(tag_name)s [slug] as [var_name] %%}" % {"tag_name": token.contents.split()[0]}
+            
+    if slug[0] == slug[-1] and slug[0] in ('"', "'"):
+        slug = slug[1:-1] #slug came through in quotes, strip them away
+    
+    activity = Activity.objects.get(slug=slug)
+    return ActivityNode(activity, var_name)
