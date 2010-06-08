52f0b27e270210480dbd232d9b72464d9c0c8b42
Eric Buckley <eric.buckley@gmail.com>
2010-06-08 10:50:21 -0400

    added first name, last name and email to the listing view of the user admin page.

--- rah/admin.py
+++ rah/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 from models import Profile
 from forms import ProfileEditForm
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email",)
+    ordering = ("id",)
+
 class ProfileAdmin(admin.ModelAdmin):
     form = ProfileEditForm
 
+admin.site.register(User, UserAdmin)
 admin.site.register(Profile, ProfileAdmin)
