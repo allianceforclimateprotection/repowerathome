6c8a641c771e420813e15c1f4059c3a2151884c5
Jon Lesser <jon.lesser@climateprotect.org>
2010-06-09 00:12:06 -0400

    missing comma in settings.py. Didn't notice because over ridden by local_settings.py (this is why you don't edit on the live site)

--- settings.py
+++ settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware'
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'django.middleware.transaction.TransactionMiddleware',
 )
 
