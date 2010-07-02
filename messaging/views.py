7e596278b315efeaedfa08576ed8bd8675e2dae9
Eric Buckley <eric.buckley@gmail.com>
2010-07-01 21:47:18 -0400

    created a messaging opens view to track how many times an email is opened. [completed: 378]

--- messaging/views.py
+++ messaging/views.py
@@ -1,5 +1,18 @@
 
 from django.http import HttpResponse
+from django.template import loader, Context
 
-def open(request):
-    return HttpResponse()
+from models import RecipientMessage
+
+def open(request, token):
+    try:
+        recipient_message = RecipientMessage.objects.get(token=token)
+        recipient_message.opens += 1
+        recipient_message.save()
+    except RecipientMessage.DoesNotExist:
+        # TODO: an invalid token was passed back, we should track this in some log
+        pass
+    response = HttpResponse(mimetype="image/gif")
+    t = loader.get_template("messaging/open_tracker.gif")
+    response.write(t.render(Context({})))
+    return response
