2e4691f3885de3e410e5aedf3087838cbffb4d01
Eric Buckley <eric.buckley@gmail.com>
2010-07-06 14:03:07 -0400

    command was written in the wrong format, fixed accordingly

--- messaging/management/commands/send_ready_messages.py
+++ messaging/management/commands/send_ready_messages.py
@@ -1,6 +1,8 @@
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 
 from messaging.models import Queue
 
-class Command(NoArgsCommand):
-    Queue.objects.send_ready_messages()
+class Command(BaseCommand):
+    
+    def handle(self, *args, **options):
+        Queue.objects.send_ready_messages()
