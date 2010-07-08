7e5e3a2da32a4a5a80efa10fe386fe35b6e6a290
Eric Buckley <eric.buckley@gmail.com>
2010-07-08 17:31:14 -0400

    added custom admin view for ABTest

--- messaging/admin.py
+++ messaging/admin.py
@@ -4,8 +4,11 @@ from models import Message, ABTest, Stream
 
 class MessageAdmin(admin.ModelAdmin):
     list_display = ("__unicode__", "sends", "unique_opens")
+    readonly_fields = ("sends", "recipient_function")
     
+class ABTestAdmin(admin.ModelAdmin):
+    list_display = ("__unicode__", "control_sends", "control_opens", "test_sends", "test_opens")
 
 admin.site.register(Message, MessageAdmin)
-admin.site.register(ABTest)
+admin.site.register(ABTest, ABTestAdmin)
 admin.site.register(Stream)
