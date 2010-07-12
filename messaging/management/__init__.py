8d509f2d5ad213957f6ea0181531402241a54d07
Eric Buckley <eric.buckley@gmail.com>
2010-07-12 11:07:32 -0400

    fixed initial fixtures for the messaging app

--- messaging/management/__init__.py
+++ messaging/management/__init__.py
@@ -6,6 +6,7 @@ def create_default_messages(app, created_models, verbosity, **kwargs):
     from messaging.models import Message, ABTest
     from django.core.management import call_command
     if Message in created_models and ABTest in created_models and kwargs.get('interactive', True):
+        call_command("loaddata", "messaging/fixtures/initial_data.json", verbosity=0, interactive=True)
         msg = "\nYou just installed the message and ab_test tables, would you like to install the " \
                 "default set of messages for commitment streams? (yes/no): "
         confirm = raw_input(msg)
@@ -14,7 +15,7 @@ def create_default_messages(app, created_models, verbosity, **kwargs):
                 confirm = raw_input('Please enter either "yes" or "no": ')
                 continue
             if confirm == 'yes':
-                call_command("loaddata", "messages/fixtures/commitments.json", verbosity=0, interactive=True)
+                call_command("loaddata", "messaging/fixtures/commitment.json", verbosity=0, interactive=True)
             break
 print("setting up signal")
 signals.post_syncdb.connect(create_default_messages, sender=messaging_app, 
