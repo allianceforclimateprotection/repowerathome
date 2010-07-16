8de0d106cc7120f1cc78beff0fb3c6e9c2b3c58a
Eric Buckley <eric.buckley@gmail.com>
2010-07-16 10:32:40 -0400

    updated the comments listing page in the backend.

--- rah/admin.py
+++ rah/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group, Permission
+from django.contrib.comments.models import Comment
 
 from models import Profile
 from forms import ProfileEditForm
@@ -15,3 +16,12 @@ admin.site.register(User, UserAdmin)
 admin.site.register(Group)
 admin.site.register(Permission)
 admin.site.register(Profile, ProfileAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "parent", "comment", "submit_date",)
+    
+    def parent(self, obj):
+        return str(obj.content_object)
+    parent.short_description = "Parent"
+
+admin.site.register(Comment, CommentAdmin)
