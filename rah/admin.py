47f2d70cf8ccbf768141b17d3427b8d33e4c6c71
Eric Buckley <eric.buckley@gmail.com>
2010-07-16 14:28:19 -0400

    added likes and dislikes columns to the comments listing page in the backend

--- rah/admin.py
+++ rah/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group, Permission
 from django.contrib.comments.models import Comment
+from django.contrib.contenttypes.models import ContentType
 
 from models import Profile
 from forms import ProfileEditForm
+from rateable.models import Rating
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ("first_name", "last_name", "email",)
@@ -18,10 +20,24 @@ admin.site.register(Permission)
 admin.site.register(Profile, ProfileAdmin)
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ("user_name", "parent", "comment", "submit_date",)
+    list_display = ("user_name", "parent", "comment", "likes", "dislikes", "submit_date",)
+    
+    def __init__(self, *args, **kwargs):
+        super(CommentAdmin, self).__init__(*args, **kwargs)
+        self.content_type = ContentType.objects.get_for_model(Comment)
     
     def parent(self, obj):
         return str(obj.content_object)
     parent.short_description = "Parent"
+    
+    def likes(self, obj):
+        return Rating.objects.filter(content_type=self.content_type, object_pk=obj.id,
+            score=1).count()
+    likes.short_description = "Likes"
+    
+    def dislikes(self, obj):
+        return Rating.objects.filter(content_type=self.content_type, object_pk=obj.id,
+            score=0).count()
+    dislikes.short_description = "Dislikes"
 
 admin.site.register(Comment, CommentAdmin)
