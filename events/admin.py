2581ba6caa40398cddca8e2775b4e14ed8a57052
Eric Buckley <eric.buckley@gmail.com>
2010-07-19 12:36:58 -0400

    added the event model to the backend

--- events/admin.py
+++ events/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from models import EventType
+from models import EventType, Event
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("__unicode__", "place_name", "start_datetime", "is_private")
+    readonly_fields = ("limit",)
 
 admin.site.register(EventType)
+admin.site.register(Event, EventAdmin)
