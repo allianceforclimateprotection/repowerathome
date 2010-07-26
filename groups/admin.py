76d96e00da1dc035338ffc527725be39a61cb17d
Eric Buckley <eric.buckley@gmail.com>
2010-07-26 11:27:18 -0400

    updated teams listing page in the backend [completed: 421]

--- groups/admin.py
+++ groups/admin.py
@@ -2,9 +2,22 @@ from django.contrib import admin
 from models import Group, GroupUsers
 
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ("name", "is_geo_group",)
-    list_filter = ("is_geo_group",)
+    list_display = ("name", "managers", "total_members", "committed_actions", "completed_actions", "total_points",)
     readonly_fields = ("is_geo_group", "location_type", "sample_location", "parent",)
+    
+    def queryset(self, request):
+        qs = super(GroupAdmin, self).queryset(request)
+        return qs.filter(is_geo_group=False)
+    
+    def managers(self, obj):
+        return ", ".join([u.user.get_full_name() for u in GroupUsers.objects.filter(group=obj, is_manager=True)])
+    managers.short_description = "Managers"
+    
+    def committed_actions(self, obj):
+        return sum([m.actions_committed for m in obj.members_ordered_by_points() if m.actions_committed])
+        
+    def completed_actions(self, obj):
+        return sum([m.actions_completed for m in obj.members_ordered_by_points() if m.actions_completed])
 
 admin.site.register(Group, GroupAdmin)
 admin.site.register(GroupUsers)
