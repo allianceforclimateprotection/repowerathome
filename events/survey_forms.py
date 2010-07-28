d9869add3cfbe0d2dfd434e741cb2a750e2e3b4a
Jon Lesser <jon.lesser@climateprotect.org>
2010-07-28 16:41:29 -0400

    Adding van import command and associated data files [Completed: 356]

--- events/survey_forms.py
+++ events/survey_forms.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from django import forms
 from django.core.urlresolvers import reverse
 from django.template.loader import render_to_string
@@ -64,6 +66,81 @@ class EnergyMeetingCommitmentCard(SurveyForm):
         required=False, label="Have an energy audit")
     host_event = forms.BooleanField(required=False, label="Host an event")
     join_team = forms.BooleanField(required=False, label="Join a team")
+
+class PilotEnergyMeetingCommitmentCard(SurveyForm):
+    CHOICES = (
+        ("D", "I've Done this"),
+        ("C", "I pledge to do this"),
+    )
+    replace_hvac_filter = ActionChoiceField(action_slug="change-air-conditioning-heater-filters",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Change your HVAC filter regularly")
+    replace_old_fridge = ActionChoiceField(action_slug="replace-your-outdated-refrigerator",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Replace your outdated refrigerator ")
+    retire_refrigerat = ActionChoiceField(action_slug="retire-second-refrigerator",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Retire your second refrigerator")
+    seal_vents = ActionChoiceField(action_slug="seal-vents-unoccupied-rooms",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Seal off vents in unoccupied rooms")
+    use_fan_in_summer = ActionChoiceField(action_slug="use-ceiling-fan-summer",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Use ceiling fans in the summer")
+    water_heater_120deg = ActionChoiceField(action_slug="adjust-water-heater-temperature",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Set your water heater to 120°F")
+    insulate_windows = ActionChoiceField(action_slug="insulate-your-windows",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Insulate your windows")
+    insulate_wtr_pipes = ActionChoiceField(action_slug="insulate-water-pipes",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Insulate your hot water pipes")
+    lowflow_shower = ActionChoiceField(action_slug="install-low-flow-shower-head",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Install a low-flow shower head")
+    manual_thermostat = ActionChoiceField(action_slug="save-with-manual-thermostat",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Save money with a manual thermostat")
+    program_thermostat = ActionChoiceField(action_slug="programmable-thermostat",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Use a programmable thermostat")
+    replace_bulb_cfl = ActionChoiceField(action_slug="replace-your-incandescent-light-bulbs-with-cfls",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Replace five light bulbs with CFLs ")
+    energy_audit = ActionChoiceField(action_slug="have-home-energy-audit",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Have a home energy audit and retrofit")
+    fan_in_winter = ActionChoiceField(action_slug="use-ceiling-fan-winter",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Use ceiling fans in the winter")
+    inst_chm_guard = ActionChoiceField(action_slug="install-chimney-draft-guard",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Install a chimney draft guard")
+    inst_sink_aerator = ActionChoiceField(action_slug="install-low-flow-sink-aerator",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Install a low-flow sink aerator")
+    insulate_water_htr = ActionChoiceField(action_slug="insulate-water-heater",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Insulate your water heater")
+    caulk_windows = ActionChoiceField(action_slug="caulk-around-your-windows",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Caulk around your windows")
+    close_firedamper = ActionChoiceField(action_slug="keep-fireplace-damper-closed",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Keep the fireplace damper closed")
+    cold_wash_cycle = ActionChoiceField(action_slug="wash-clothes-cold-water",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Use the cold wash cycle")
+    computer_to_sleep = ActionChoiceField(action_slug="set-your-computer-sleep-automatically",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Set your computer to sleep automatically")
+    elim_always_on_light = ActionChoiceField(action_slug="sensors-timers-for-lights",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Eliminate 'always-on' lights")
+    dial_dwn_thermostat = ActionChoiceField(action_slug="dial-down-thermostat",
+        choices=CHOICES, widget=forms.RadioSelect(renderer=RadioRendererForTable),
+        required=False, label="Dial down your thermostat")     
     
 class VolunteerInterestForm(SurveyForm):
 
--- van_import/import_van_dump.py
+++ van_import/import_van_dump.py
@@ -1,18 +1,93 @@
 import csv
+from events.models import Event
 
-from ...www import local_settings
+# from ..www import local_settings
+# Note: Brenna's VAN ID is 100221441
 
 class VanImporter(object):
-    """Imports data exported from VAN"""
+    """Imports data exported from VAN. input_files should be a list of file names"""
     
-    def __init__(self, input_file):
-        self.input_file = input_file
-        self.main()
+    def __init__(self, input_files=[], event_files=[]):
+        if not input_files:
+            exit("No input files provided")
+        self.people = {}
+        self.events = {}
+        self.input_files = input_files
+        self.event_files = event_files
+        self.action_map = {'replace_hvac_filter':'change-air-conditioning-heater-filters',
+                           'replace_old_fridge':'replace-your-outdated-refrigerator',
+                           'retire_refrigerat':'retire-second-refrigerator',
+                           'seal_vents':'seal-vents-unoccupied-rooms',
+                           'use_fan_in_summer':'use-ceiling-fan-summer',
+                           'water_heater_120deg':'adjust-water-heater-temperature',
+                           'insulate_windows':'insulate-your-windows',
+                           'insulate_wtr_pipes':'insulate-water-pipes',
+                           'lowflow_shower':'install-low-flow-shower-head',
+                           'manual_thermostat':'save-with-manual-thermostat',
+                           'program_thermostat':'programmable-thermostat',
+                           'replace_bulb_cfl':'replace-your-incandescent-light-bulbs-with-cfls',
+                           'energy_audit':'have-home-energy-audit',
+                           'fan_in_winter':'use-ceiling-fan-winter',
+                           'inst_chm_guard':'install-chimney-draft-guard',
+                           'inst_sink_aerator':'install-low-flow-sink-aerator',
+                           'insulate_water_htr':'insulate-water-heater',
+                           'caulk_windows':'caulk-around-your-windows',
+                           'close_firedamper':'keep-fireplace-damper-closed',
+                           'cold_wash_cycle':'wash-clothes-cold-water',
+                           'computer_to_sleep':'set-your-computer-sleep-automatically',
+                           'dial_dwn_thermostat':'dial-down-thermostat',
+                           'elim_always_on_light':'sensors-timers-for-lights'}
         
+        self.main()
+            
     def main(self):
-        reader = csv.DictReader(open(self.input_file))
-        for row in reader:
-            print row
+        # Loop through each commitment input file
+        for f in self.input_files:
+            reader = csv.DictReader(open(f, 'rU'), dialect='excel-tab')
+            for row in reader:
+                cid = row['My Campaign ID']
+                try:
+                    self.people[cid].update(row)
+                except KeyError, e:
+                    self.people[cid] = row        
+        """
+        # How many cid's don't have events
+        print "My Campaign ID, FirstName, LastName, Email"
+        for k, v in self.people.iteritems():
+            if 'House Party Code' not in v.keys():
+                print "%s, %s, %s, %s" % (v['My Campaign ID'], v['FirstName'], v['LastName'], v['LastName'])
 
+        # How many peeps don't have commitments
+        print "My Campaign ID,House Party Code,House Party Date,Host"
+        for k, v in self.people.iteritems():
+            if 'LastName' not in v.keys():
+                # print v
+                print "%s, %s, %s, %s" % (v['My Campaign ID'], v['House Party Code'], v['House Party Date'], v['Host'])
+        """
+        
+        # Prune a few incomplete records
+        print "Pruning %s records..." % len(self.people.keys())
+        prune_keys = []
+        for k, v in self.people.iteritems():
+            if len(v.keys()) <> 40:
+                prune_keys.append(k)
+        for k in prune_keys:
+            del self.people[k]
+        print "pruned %s record(s)" % len(prune_keys)
+        
+        # Loop through the people and make a set of events
+        for k, v in self.people.iteritems():
+            self.events[v['House Party Code']] = v['House Party Date']
+        
+        print "There are %s event(s)" % len(self.events.keys())
+        
 if __name__ == "__main__":
-    VanImporter(input_file="2.csv")
+    input_files = [
+        "commitments1.txt", 
+        "commitments2.txt", 
+        "commitments3.txt", 
+        "commitments4.txt", 
+        "events.txt", 
+        "emails.txt"
+    ]
+    VanImporter(input_files=input_files, event_files=['events.txt'])
