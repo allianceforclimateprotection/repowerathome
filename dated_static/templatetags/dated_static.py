8a3eab5caa2fc6325ea64504ac0966a9d9b70815
Eric Buckley <eric.buckley@gmail.com>
2010-08-12 16:46:35 -0400

    if we can't find the static file, return the path of the static file prefixed with MEDIA_URL

--- dated_static/templatetags/dated_static.py
+++ dated_static/templatetags/dated_static.py
@@ -13,4 +13,4 @@ def dated_static(static_file):
         m_time = int(os.stat(abs_path).st_mtime)
         return os.path.join(settings.MEDIA_URL, os.path.join(paths[0], "%s?%s" % (paths[1], m_time)))
     except:
-        return static_file
+        return os.path.join(settings.MEDIA_URL, static_file)
