6543a054ea45873c5d533a17100e72e46d275e6b
Eric Buckley <eric.buckley@gmail.com>
2010-08-16 13:49:56 -0400

    added a SmartCrop processor based off of easy_thumbnails processor

--- thumbnails/processors.py
+++ thumbnails/processors.py
@@ -1,3 +1,4 @@
+import math
 import re
 
 from PIL import Image, ImageFilter
@@ -16,34 +17,53 @@ class Processor(object):
 class Resize(Processor):
     @classmethod
     def key_expression(cls):
-        return re.compile("(\d+)x(\d+)", re.IGNORECASE)
+        return re.compile("^(\d+)x(\d+)$", re.IGNORECASE)
         
     @classmethod
     def process(cls, image, matcher):
         width = int(matcher.group(1))
         height = int(matcher.group(2))
         image.thumbnail((width,height), Image.ANTIALIAS)
+        return image
         
 class Detail(Processor):
     @classmethod
     def key_expression(cls):
-        return re.compile("detail", re.IGNORECASE)
+        return re.compile("^detail$", re.IGNORECASE)
         
     @classmethod
     def process(cls, image, matcher):
         try:
-            image = image.filter(ImageFilter.DETAIL)
+            return image.filter(ImageFilter.DETAIL)
         except ValueError:
             pass
+        return image
         
 class Sharpen(Processor):
     @classmethod
     def key_expression(cls):
-        return re.compile("sharpen", re.IGNORECASE)
+        return re.compile("^sharpen$", re.IGNORECASE)
 
     @classmethod
     def process(cls, image, matcher):
         try:
-            image = image.filter(ImageFilter.SHARPEN)
+            return image.filter(ImageFilter.SHARPEN)
         except ValueError:
             pass
+        return image
+            
+class SmartCrop(Processor):
+    """
+    Note this processor has a dependancy on the easy-thumbnails library
+    """
+    @classmethod
+    def key_expression(cls):
+        return re.compile("^(\d+)x(\d+)smartcrop$", re.IGNORECASE)
+        
+    @classmethod
+    def process(cls, image, matcher):
+        from easy_thumbnails.processors import scale_and_crop
+        width = int(matcher.group(1))
+        height = int(matcher.group(2))
+        return scale_and_crop(im=image, size=(width,height), crop="smart")
+            
