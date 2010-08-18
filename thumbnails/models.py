416d52c9247081ed74130fffd987fe9f5e523d60
Eric Buckley <eric.buckley@gmail.com>
2010-08-18 16:12:47 -0400

    updated the thumbnails app to store a list of thumbnails in the database, the hope is this list can be referenced faster than actually touching the filesystem

---
+++ thumbnails/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class ThumbnailManager(models.Manager):
+    def thumbnails_for(self, raw):
+        return self.filter(raw=raw).values_list("thumbnail", flat=True)
+
+class Thumbnail(models.Model):
+    raw = models.CharField(max_length=255, db_index=True)
+    thumbnail = models.CharField(max_length=255)
+    objects = ThumbnailManager()
+    
+    class Meta:
+        unique_together = ("raw", "thumbnail",)
