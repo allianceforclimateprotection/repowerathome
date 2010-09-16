373fb12244f32416bfa157a6aa094dc13947cf83
Eric Buckley <eric.buckley@gmail.com>
2010-09-16 11:17:30 -0400

    added more columns to the events admin page per Shira's suggestion

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.utils.dateformat import DateFormat
 
-from models import EventType, Event
+from models import EventType, Event, Guest
 from forms import EventForm
 
 class EventAdminForm(EventForm):
@@ -16,10 +17,31 @@ class EventAdminForm(EventForm):
         return super(EventForm, self).save(*args, **kwargs)
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ("__unicode__", "place_name", "start_datetime", "is_private",)
+    list_display = ("name", "_when", "city", "state", "hosts", "commitment_cards_entered", "is_private",)
     list_filter = ("event_type", "location",)
+    date_hierarchy = "when"
     readonly_fields = ("limit",)
     form = EventAdminForm
+    
+    def name(self, obj):
+        return obj.place_name or obj.__unicode__()
+    
+    def _when(self, obj):
+        return DateFormat(obj.start_datetime()).format("M j Y @ g:ia")
+    _when.short_description = "When"
+    
+    def city(self, obj):
+        return obj.location.name
+        
+    def state(self, obj):
+        return obj.location.st
+        
+    def commitment_cards_entered(self, obj):
+        return Guest.objects.distinct().filter(event=obj, commitment__isnull=False).count()
+    
+    def hosts(self, obj):
+        return ", ".join([u.get_full_name() for u in obj.hosts()])
+    hosts.short_description = "Hosts"
 
 admin.site.register(EventType)
 admin.site.register(Event, EventAdmin)
