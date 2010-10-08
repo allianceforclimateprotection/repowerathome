ef79a4e8ea55963004c804894dc48adda0e86a2c
Jon Lesser <jon.lesser@climateprotect.org>
2010-10-08 16:41:03 -0400

    Commitment cards submit and save.

--- commitments/forms.py
+++ commitments/forms.py
@@ -6,13 +6,21 @@ from models import Contributor
 class ContributorForm(forms.ModelForm):
     
     zipcode = forms.CharField(max_length=10, required=False, help_text="Leave blank if not a US resident")
-    # email = forms.EmailField(label='Email', widget=forms.TextInput)
+    email = forms.EmailField(label='Email', widget=forms.TextInput, required=False)
     first_name = forms.CharField(min_length=2)
     
     class Meta:
         model = Contributor
         fields = ('first_name', 'last_name', 'email', 'phone')
     
+    def save(self):
+        import pdb
+        pdb.set_trace()
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        return email
+                
     def clean_zipcode(self):
         data = self.cleaned_data['zipcode'].strip()
         if not len(data):
