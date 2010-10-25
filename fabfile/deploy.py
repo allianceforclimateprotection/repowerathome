507d51a59be6c8ea262cf263682b15695db69b07
Eric Buckley <eric.buckley@gmail.com>
2010-10-25 16:05:02 -0400

    created wrapper for making a code deploy, that is just running the git commands on the server

--- fabfile/deploy.py
+++ fabfile/deploy.py
@@ -16,59 +16,66 @@ def enable_maintenance_page():
                 sudo("ln -s /etc/nginx/sites-available/maintenance /etc/nginx/sites-enabled/maintenance")
                 sudo("/etc/init.d/nginx reload")
     
-def fetch():
+def _fetch():
     "Updates the application with new code"
-    require("hosts", provided_by=deployments)
+    require("hosts", "deploy_to")
     run("cd %(deploy_to)s && git fetch --all" % env)
     
-def reset():
+def _reset():
     "Set the workspace to the desired revision"
-    require("hosts", provided_by=deployments)
+    require("hosts", "deploy_to")
     run("cd %(deploy_to)s && git reset --hard" % env)
     
-def checkout():
+def _checkout():
     "Checkout the revision you would like to deploy"
-    require("hosts", provided_by=deployments)
+    require("hosts", "deploy_to")
     with settings(warn_only=True):
         result = run("cd %(deploy_to)s && git checkout -b \
             `date +'%%Y-%%m-%%d_%%H-%%M-%%S'`_`whoami` %(sha)s" % env)
         if result.failed:
             abort(red("Have you pushed your latest changes to the repository?"))
+            
+def code_deploy():
+    "Wrapper for performing a code deploy of fetch, reset and checkout"
+    require("hosts", "deploy_to")
+    _fetch()
+    _reset()
+    _checkout()
 
 def install_requirements():
     "Using pip install all of the requirements defined"
-    require("hosts", provided_by=deployments)
+    require("hosts", "deploy_to")
     sudo("cd %(deploy_to)s/../requirements && pip install -r %(deploy_to)s/requirements.txt" % env)
 
 @runs_once
 def minify():
     "Minify the js and css files"
-    require("hosts", provided_by=deployments)
+    require("hosts", "deploy_to")
     run("cd %(deploy_to)s/static/minify && ./minify.sh" % env)
     
 @runs_once
 def s3sync():
     "Sync static data with our s3 bucket"
-    require("hosts", provided_by=deployments)
+    require("hosts", "deploy_to")
     run("cd %(deploy_to)s && python manage.py sync_media_s3 --gzip --force --expires" % env)
     
 @runs_once
 def syncdb():
     "Sync the database with any new models"
-    require("hosts", provided_by=deployments)
+    require("hosts", "deploy_to")
     if confirm("This script cannot handle interactive shells, are you sure you want to run syncdb?"):
         run("cd %(deploy_to)s && python manage.py syncdb" % env)
 
 @runs_once
 def migratedb():
     "Migrate the database"
-    require("hosts", provided_by=deployments)
+    require("hosts", "deploy_to")
     if confirm("Would you like to migrate the database?"):
         run("cd %(deploy_to)s && python manage.py migrate" % env)
     
 def restart_apache():
     "Reboot Apache2 server."
-    require("hosts", provided_by=deployments)
+    require("hosts")
     sudo("/etc/init.d/apache2 restart")
 
 @runs_once
@@ -86,11 +93,9 @@ def deploy(revision=None, sync_media=True, code_only=False):
     if revision:
         env.revision = revision
         env.sha = query_revision(revision)
-    require("deploy_to", provided_by=deployments)
+    require("deploy_to", "hosts", "environment")
     enable_maintenance_page()
-    fetch()
-    reset()
-    checkout()
+    code_deploy()
     if not code_only:
         install_requirements()
         minify()
