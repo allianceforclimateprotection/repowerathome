f44e7cccf76776b37b1079e1f93660fe87345374
Eric Buckley <eric.buckley@gmail.com>
2010-11-01 11:01:16 -0400

    added a view decorator to make sure requests come via ajax calls

--- utils.py
+++ utils.py
@@ -41,3 +41,21 @@ def hex_to_byte(hex_str):
     for i in range(0, len(hex_str), 2):
         byte_list.append(chr(int(hex_str[i:i+2], 16)))
     return ''.join(byte_list)
+
+def ajax_required(f):
+    """
+    AJAX request required decorator
+    use it in your views:
+
+    @ajax_required
+    def my_view(request):
+        ....
+
+    """
+    def wrap(request, *args, **kwargs):
+            if not request.is_ajax():
+                return forbidden(request)
+            return f(request, *args, **kwargs)
+    wrap.__doc__=f.__doc__
+    wrap.__name__=f.__name__
+    return wrap
