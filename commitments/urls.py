6b067519534da70db98f670103006dc2cfe4c329
Eric Buckley <eric.buckley@gmail.com>
2010-11-04 17:20:15 -0400

    added pledge views and models so a user can save commit to the Repower at Home pledge [completed: 523]

--- commitments/urls.py
+++ commitments/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('commitments.views',
     url(r'^card/(?P<contrib_id>\d+)/$', 'card', name='commitments_card'),
     url(r'^card/(?P<contrib_id>\d+)/(?P<form_name>\w+)/$', 'card', name='commitments_card_with_form'),
     url(r'^card/(?P<form_name>\w+)/$', 'card', name='new_commitment_card_with_form'),
+    url(r'^take_pledge/$', 'take_pledge', name='take_pledge'),
 )
