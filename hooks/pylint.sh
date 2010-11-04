91f00cb6f85b1a57698840e78d107ff0e76d6b2d
Jon Lesser <jon.lesser@climateprotect.org>
2010-11-04 16:31:18 -0400

    adding a pylint rc file

--- hooks/pylint.sh
+++ hooks/pylint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+LINK=`readlink $0`
+if [ -z $LINK ]
+then
+    THIS_DIR=`cd $(dirname $0) && pwd -P`
+else
+    THIS_DIR=`cd $(dirname $0)/$(dirname $LINK) && pwd -P`
+fi
 
 for file in `git diff --cached --name-only | grep .py$`
-do
-    if ! pylint -E --generated-members=objects,DoesNotExist "$file"
+do  
+    if ! pylint -E --rcfile="$THIS_DIR/pylint.ini" "$file"
     then
         echo "$file did not pass the pylint tests"
         exit 1
