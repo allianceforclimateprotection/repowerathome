95f1689914a0a6cc4de4c644955e064eeae93214
Jon Lesser <jon.lesser@climateprotect.org>
2010-11-08 14:24:28 -0500

    Creating a community actions landing page [touch: 524] Recast commitments page as pledge card page [Completed: 527]

--- actions/urls.py
+++ actions/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('actions.views',
     url(r'^$', 'action_show', name='action_show'),
+    url(r'^community/$', 'community_show', name='action_community_show'),
     url(r'^tag/(?P<tag_slug>\w+)/$', 'action_show', name='action_tag'),
     url(r'^(?P<action_slug>[a-z0-9-]+)/$', 'action_detail', name='action_detail'),
     url(r'^(?P<action_slug>[a-z0-9-]+)/complete/$', 'action_complete', name='action_complete'),
