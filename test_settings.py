ed253de35174871395feb6dd3c3c2da3eb523eca
Eric Buckley <eric.buckley@gmail.com>
2010-11-08 17:29:02 -0500

    adding django.contrib.markup to test_settings.py

--- test_settings.py
+++ test_settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.sitemaps',
     'django.contrib.flatpages',
+    'django.contrib.markup',
     'rah',
     'rateable',
     'rateable.tests',
