b480df83534ad219e5a82ab1f0c03bc3ca2995f7
Jon Lesser <jon.lesser@climateprotect.org>
2010-11-19 17:36:44 -0500

    Forgot to rename a variable.

--- fabfile/server.py
+++ fabfile/server.py
@@ -116,7 +116,7 @@ def launch_server(cloud_name, environment="staging", instance_type="t1.micro", a
         _bootstrap(shell_vars=shell_vars, 
             command="bootstrap_system; bootstrap_database; bootstrap_appserver; bootstrap_loadbalancer;")
         install_requirements()
-    _print_mysqlduplicate_alias(name, db_password, "127.0.0.1")
+    _print_mysqlduplicate_alias(cloud_name, db_password, "127.0.0.1")
 
 def launch_cloud(cloud_name, environment="staging", count=1, lb_type="t1.micro", app_type="t1.micro", 
         rds_type="db.m1.small", ami=AMIs["ubuntu-10.10-64"]):
