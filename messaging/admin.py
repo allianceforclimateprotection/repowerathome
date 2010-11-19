30b18a7694043185d89f5810bf555bf413fe5ce3
Eric Buckley <eric.buckley@gmail.com>
2010-11-18 19:19:25 -0500

    added an action to the stream change list so staff memebers can preview all of the messages in the stream, note for the time being this will only preview the control messages defined in the Streams ABTests. [completed: 557]

--- messaging/admin.py
+++ messaging/admin.py
@@ -8,11 +8,20 @@ from models import Message, ABTest, Stream
 
 def preview_messages(modeladmin, request, queryset):
     previews = []
-    for obj in queryset:
-        for preview in obj.clean():
-            previews.append((obj.name, preview.email.body))
-    return render_to_response("messaging/previews.html", {"previews": previews}, RequestContext(request))
+    for message in queryset:
+        for preview in message.clean():
+            previews.append((message.name, preview.email.body))
+    return render_to_response("messaging/previews.html", {"previews": previews, "appmodel": "Messages"}, RequestContext(request))
 preview_messages.short_description = "Preview Messages"
+
+def preview_stream(modeladmin, request, queryset):
+    previews = []
+    for stream in queryset:
+        for abtest in stream.abtest_set.all():
+            for preview in abtest.message.clean():
+                previews.append((abtest.message.name, preview.email.body))
+    return render_to_response("messaging/previews.html", {"previews": previews, "appmodel": "Streams"}, RequestContext(request))
+preview_stream.short_description = "Preview Stream"
 
 class MessageAdmin(admin.ModelAdmin):
     list_display = ("__unicode__", "sends", "unique_opens", "click_thrus")
@@ -28,7 +37,10 @@ class MessageAdmin(admin.ModelAdmin):
     
 class ABTestAdmin(admin.ModelAdmin):
     list_display = ("__unicode__", "control_sends", "control_opens", "test_sends", "test_opens")
+    
+class StreamAdmin(admin.ModelAdmin):
+    actions = [preview_stream]
 
 admin.site.register(Message, MessageAdmin)
 admin.site.register(ABTest, ABTestAdmin)
-admin.site.register(Stream)
+admin.site.register(Stream, StreamAdmin)
