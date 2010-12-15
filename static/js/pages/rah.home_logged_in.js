919e3c2925618f5872295f34419e982c42da1986
Eric Buckley <eric.buckley@gmail.com>
2010-12-15 11:45:24 -0500

    renamed pledge module submit_setup method to setup

--- static/js/pages/rah.home_logged_in.js
+++ static/js/pages/rah.home_logged_in.js
@@ -2,25 +2,25 @@
 /*global $: false, RAH: false, FB: false, WebFont: false, jQuery: false, window: false, google: false, require: false, define: false */
 require(["mods/pledge", "mods/invite", "libs/jquery.validate", "libs/jquery.ui", "mods/commitments"],
     function (pledge, invite, validate, ui, commitments) {
-        pledge.submit_setup();
+        pledge.setup();
         invite.setup();
          // Setup twitter update form, link, and dialog
         $("#twitter_status_form").validate({ rules: { status: { required: true }}});
-        $('#twitter_status_link').click(function () {
-            $('#twitter_post_dialog').dialog('open'); 
+        $("#twitter_status_link").click(function () {
+            $("#twitter_post_dialog").dialog("open"); 
             return false;
         });
         
         // Create an object for the buttons
         var buttons = { };
-        if ($('#twitter_status_form').length) {
+        if ($("#twitter_status_form").length) {
             buttons["Update status"] = function () { 
-                $('#twitter_status_form').submit(); 
+                $("#twitter_status_form").submit(); 
             };
         }
         
-        $('#twitter_post_dialog').dialog({
-            title: 'Tell your tweeps about us', 
+        $("#twitter_post_dialog").dialog({
+            title: "Tell your tweeps about us", 
             modal: true, 
             autoOpen: false,
             buttons: buttons
