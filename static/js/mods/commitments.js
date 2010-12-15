8bb81fa4207595bb6e9feeea8555bb87fc5eee3b
Eric Buckley <eric.buckley@gmail.com>
2010-12-15 15:06:40 -0500

    the commitments mod was referencing another public facing method using this "this" keyword within an anonymous function, because this doesnt work we need to redesign the module using the revealing module pattern

--- static/js/mods/commitments.js
+++ static/js/mods/commitments.js
@@ -2,19 +2,44 @@
 /*global $: false, FB: false, WebFont: false, jQuery: false, window: false, google: false, require: false, define: false */
 define(["libs/jquery.ui", "mods/messages", "libs/jquery.validation"], 
     function (ui, messages, validate) {
+        var link_setup = function () {
+            $(".commit_action").click(function () {
+                var elem = $(this);
+                elem.siblings("span.commit_tick")
+                    .toggleClass("ui-icon-triangle-1-e")
+                    .toggleClass("ui-icon-triangle-1-s");
+                $("td .commit_list_ul_" + elem.attr("id").split("__")[1]).toggle();
+            });
+            $(".commit_list_ul li").hover(function () {
+                $(this).children(".commit_user_list_edit_link").toggle();
+            });
+        },
+        form_setup = function (action) {
+            $("#commitment_card_form .commit_card_choice input").live("click", function () {
+                var id = $(this).attr("id");
+                var index = id.substr(id.length - 1, 1);
+                var other_index = (index === "1") ? "0" : "1";
+                var field_name = id.substr(0, id.length - 1);
+                $("#" + field_name + other_index).attr("checked", false);
+            });
+            
+            $("#commitment_card_form").validate({
+                rules: {
+                    zipcode:    { required: false, minlength: 5, digits: true },
+                    email:      { required: false, email: true },
+                    first_name: { required: true, minlength: 2 }
+                }
+            });
+            
+            $("#commitment_card_form").attr("action", action);
+            
+            $("#commitment_card_select_form #id_form_name").change(function () {
+                var load_url = action + '?form_name=' + $(this).val();
+                $("#commitment_card_action_table").load(load_url + ' #commitment_card_action_table table');
+                $("#commitment_card_form").attr("action", load_url);
+            });
+        };
         return {
-            link_setup: function () {
-                $(".commit_action").click(function () {
-                    var elem = $(this);
-                    elem.siblings("span.commit_tick")
-                        .toggleClass("ui-icon-triangle-1-e")
-                        .toggleClass("ui-icon-triangle-1-s");
-                    $("td .commit_list_ul_" + elem.attr("id").split("__")[1]).toggle();
-                });
-                $(".commit_list_ul li").hover(function () {
-                    $(this).children(".commit_user_list_edit_link").toggle();
-                });
-            },
             card_setup: function () {
                 $("#commitment_card_dialog").remove();
                 $('body').append('<div id="commitment_card_dialog"></div>');
@@ -27,7 +52,7 @@ define(["libs/jquery.ui", "mods/messages", "libs/jquery.validation"],
                                     $("#commitment_card_dialog").dialog("close");
                                     messages.add_message(messages_html);
                                     $("#commitments_show_table").load('/commitments/ #commitments_show_table table', function () {
-                                        this.link_setup();
+                                        link_setup();
                                     });
                                 }
                             });
@@ -40,38 +65,14 @@ define(["libs/jquery.ui", "mods/messages", "libs/jquery.validation"],
                 // Attach functionality commitment card links
                 $(".commitment_card_open").click(function () {
                     var href = $(this).attr('href');
-                    
                     $("#commitment_card_dialog").load(href, function () {
-                        this.form_setup(href);
+                        form_setup(href);
                         $("#commitment_card_dialog").dialog("open");
                     });
                     return false;
                 });
             },
-            form_setup: function (action) {
-                $("#commitment_card_form .commit_card_choice input").live("click", function () {
-                    var id = $(this).attr("id");
-                    var index = id.substr(id.length - 1, 1);
-                    var other_index = (index === "1") ? "0" : "1";
-                    var field_name = id.substr(0, id.length - 1);
-                    $("#" + field_name + other_index).attr("checked", false);
-                });
-                
-                $("#commitment_card_form").validate({
-                    rules: {
-                        zipcode:    { required: false, minlength: 5, digits: true },
-                        email:      { required: false, email: true },
-                        first_name: { required: true, minlength: 2 }
-                    }
-                });
-                
-                $("#commitment_card_form").attr("action", action);
-                
-                $("#commitment_card_select_form #id_form_name").change(function () {
-                    var load_url = action + '?form_name=' + $(this).val();
-                    $("#commitment_card_action_table").load(load_url + ' #commitment_card_action_table table');
-                    $("#commitment_card_form").attr("action", load_url);
-                });
-            }
+            link_setup: link_setup,
+            form_setup: form_setup
         };
     });
