451c158da1128eb28907a53bfd83aa3742720908
Eric Buckley <eric.buckley@gmail.com>
2010-12-15 12:14:02 -0500

    fixed all jquery.validate typos and replaced with jquery.validation

--- static/js/mods/share.js
+++ static/js/mods/share.js
@@ -1,6 +1,6 @@
 /*jslint maxerr: 1000, white: true, browser: true, devel: true, rhino: true, onevar: false, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true, sub: true */
 /*global $: false, RAH: false, FB: false, WebFont: false, jQuery: false, window: false, google: false, require: false, define: false */
-define(["libs/jquery.ui", "libs/facebook", "libs/jquery.validate", "mods/messagses"],
+define(["libs/jquery.ui", "libs/facebook", "libs/jquery.validation", "mods/messagses"],
     function (ui, facebook, validate, messages) {
         return {
             init: function (url) {
