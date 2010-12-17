7f51591a9d8bd3e4c7ec9da6aa5f49cba14b435e
Jon Lesser <jon.lesser@climateprotect.org>
2010-12-17 11:36:21 -0500

    Adding approved column to admin

--- media_widget/admin.py
+++ media_widget/admin.py
@@ -4,6 +4,6 @@ from models import StickerImage
 class StickerImageAdmin(admin.ModelAdmin):
     search_fields = ("name", "description", "email",)
     ordering = ("-created",)
-    list_display = ("name", "email", "description", "created",)
+    list_display = ("name", "email", "description", "created", "approved",)
 
 admin.site.register(StickerImage, StickerImageAdmin)
