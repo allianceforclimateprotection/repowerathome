c5e0778ff6a247039f0801fbc51771a688953c9c
Eric Buckley <eric.buckley@gmail.com>
2010-12-18 12:33:19 -0500

    the validation library is needed in base.js

--- tools/app.build.js
+++ tools/app.build.js
@@ -6,7 +6,7 @@
     modules: [
         {
             name: "pages/base",
-            include: ["libs/webfont", "libs/jquery.ui", "libs/jquery.form", "mods/feedback", "mods/messages", "mods/facebook", "pages/base"]
+            include: ["libs/webfont", "libs/jquery.ui", "libs/jquery.form", "libs/jquery.validation", "mods/feedback", "mods/messages", "mods/facebook", "pages/base"]
         },
         {
             name: "pages/rah.home_logged_out",
@@ -16,22 +16,19 @@
         {
             name: "pages/rah.home_logged_in",
             exclude: ["pages/base"],
-            include: ["mods/pledge", "libs/jquery.validation", "mods/invite", "mods/commitments"]
+            include: ["mods/pledge", "mods/invite", "mods/commitments"]
         },
         {
             name: "pages/rah.login",
             exclude: ["pages/base"],
-            include: ["libs/jquery.validation"]
         },
         {
             name: "pages/rah.register",
             exclude: ["pages/base"],
-            include: ["libs/jquery.validation"]
         },
         {
             name: "pages/rah.profile.edit",
             exclude: ["pages/base"],
-            include: ["libs/jquery.validation"]
         }
     ]
 })
