bd6e35f82236389181e8792bf297a717749d1496
Eric Buckley <eric.buckley@gmail.com>
2011-01-18 11:20:12 -0500

    completed an search widget for the events landing page, complete with autocomplete functionality. however it has yet to be styled [completed: 605]

--- static/js/pages/events.show.js
+++ static/js/pages/events.show.js
@@ -1,6 +1,6 @@
 /*jslint maxerr: 1000, white: true, browser: true, devel: true, rhino: true, onevar: false, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true, sub: true */
 /*global $: false, RAH: false, FB: false, WebFont: false, jQuery: false, window: false, google: false, require: false, define: false */
-require(["libs/jquery.validation", "libs/jquery.ui", "mods/search"],
+require(["libs/jquery.validation", "libs/jquery.ui"],
     function (validation, ui, search) {
         var form = $('.search_widget');
         $('#search_widget_input').autocomplete({
@@ -14,17 +14,40 @@ require(["libs/jquery.validation", "libs/jquery.ui", "mods/search"],
                     },
                     success: function (data) {
                         res($.map(data, function (item) {
-                            console.log(item.url);
                             return {
-                                label: item.zipcode,
-                                value: item.id
+                                label: item.label, 
+                                value: item.id,
+                                date: item.date,
+                                url: item.url
                             };
                         }));
                     }
                 });
             },
+            focus: function (event, ui) {
+                this.value = ui.item.label;
+                return false;
+            },
+            select: function (event, ui) {
+                window.location = ui.item.url;
+                return false;
+            },
             minLength: 2
-        });
+        /*jslint nomen: false*/
+        }).data('autocomplete')._renderItem = function (ul, item) {
+        /*jslint nomen: false*/
+            return $('<li/>')
+                .data('item.autocomplete', item)
+                .append($('<a/>', {
+                    href: item.url,
+                    text: item.label
+                }))
+                .append($('<span/>', {
+                    text: item.date,
+                    style: 'font-size: smaller'
+                }))
+                .appendTo(ul);
+        };
         $("#house_party_form").validate({ rules: { phone_number: { required: true }}});
         $('#house_party_link').click(function () { 
             $('#house_party_dialog').dialog('open'); 
