c52ec933a5ad84d9a01dda29579d8921e029093d
Eric Buckley <eric.buckley@gmail.com>
2011-01-20 15:54:06 -0500

    updated codebase calls to use api3.codebasehq.com [completed: 617]

--- fabfile/codebase.py
+++ fabfile/codebase.py
@@ -9,14 +9,15 @@ from utils import runs_last
 def _send_codebase(path, data=None):
     "POST the given data message to the codebase API, if no data is provided GET"
     match = re.search("git\@(codebasehq.*):(.*)\/(.*)\/.*\.git", env.repository)
-    domain = "%s.%s" % (match.group(2), match.group(1))
+    account = match.group(2)
+    domain = "api3.%s" % match.group(1)
     project = match.group(3)
     username = local("git config codebase.username").strip()
     api_key = local("git config codebase.apikey").strip()
     
     url = "https://%s/%s%s" % (domain, project, path)
     headers = {"Content-type": "application/xml", "Accept": "application/xml", 
-        "Authorization": "Basic %s" % base64.b64encode("%s:%s" % (username, api_key))}
+        "Authorization": "Basic %s" % base64.b64encode("%s/%s:%s" % (account, username, api_key))}
     request = urllib2.Request(url, data, headers)
     return urllib2.urlopen(request).read()
 
