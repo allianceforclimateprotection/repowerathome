ca23cba614122506b66f4ea4d4f3e41685e804a9
Eric Buckley <eric.buckley@gmail.com>
2011-01-21 12:40:21 -0500

    moved a lot of the new search widget code over to mods/search to make it a little more modular

--- static/js/mods/search.js
+++ static/js/mods/search.js
@@ -2,32 +2,65 @@
 /*global $: false, RAH: false, FB: false, WebFont: false, jQuery: false, window: false, google: false, require: false, define: false */
 define(function () {
     return {
-        setup: function () {
-            $(".search_widget").submit(function () {
-                var form = $(this);
-                $.ajax({
-                    url: form.attr("action"),
-                    type: form.attr("method"),
-                    data: form.serialize(),
-                    success: function (data) {
-                        data = jQuery.trim(data);
-                        if (data.length > 0) {
-                            $(".search_results").removeClass("hidden");
-                            $(".search_results", form).html(data);
-                        } else {
-                            $(".search_results").addClass("hidden");
+        init_widget: function () {
+            var form = $('.search_widget');
+            var widget = $('#search_widget_input').autocomplete({
+                source: function (req, res) {
+                    $.ajax({
+                        url: form.attr('action'),
+                        dataType: 'json',
+                        data: {
+                            search: req.term,
+                            format: 'json'
+                        },
+                        success: function (data) {
+                            res(data);
                         }
-                    }
-                });
-                return false;
+                    });
+                },
+                focus: function (event, ui) {
+                    this.value = ui.item.label;
+                    return false;
+                },
+                select: function (event, ui) {
+                    window.location = ui.item.url;
+                    return false;
+                },
+                minLength: 2
             });
-            $(".search_more").live("click", function () {
-                var link = $(this);
-                $.get(link.attr("href"), function (data) {
-                    link.replaceWith(data);
-                });
-                return false;
-            });
+            /*jslint nomen: false*/
+            widget.data('autocomplete')._renderItem = function (ul, item) {
+            /*jslint nomen: true*/
+                return $('<li/>')
+                    .data('item.autocomplete', item)
+                    .append($('<a/>', {
+                        href: item.url,
+                        text: item.label
+                    }))
+                    .appendTo(ul);
+            };
+            /*jslint nomen: false*/
+            widget.data('autocomplete')._renderMenu = function (ul, items) {
+            /*jslint nomen: true*/
+                var self = this;
+                if (items.length) {
+                    $.each(items, function (index, item) {
+                        self._renderItem(ul, item);
+                    });
+                } else {
+                    return $('<li/>', {
+                        text: 'No results'
+                    })
+                    .appendTo(ul);
+                }
+            };
+            widget.data('autocomplete')._response = function (content) {
+                content = this._normalize( content );
+                this._suggest( content );
+                this._trigger( "open" );
+                this.element.removeClass( "ui-autocomplete-loading" );
+            };
+            return widget;
         }
     };
 });
