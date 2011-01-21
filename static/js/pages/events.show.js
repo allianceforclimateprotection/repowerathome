62409d1568c2b897f31b5516256b45478cfcd9a9
Eric Buckley <eric.buckley@gmail.com>
2011-01-21 12:40:21 -0500

    moved a lot of the new search widget code over to mods/search to make it a little more modular

--- static/js/pages/events.show.js
+++ static/js/pages/events.show.js
@@ -1,41 +1,11 @@
 /*jslint maxerr: 1000, white: true, browser: true, devel: true, rhino: true, onevar: false, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true, sub: true */
 /*global $: false, RAH: false, FB: false, WebFont: false, jQuery: false, window: false, google: false, require: false, define: false */
-require(["libs/jquery.validation", "libs/jquery.ui"],
+require(["libs/jquery.validation", "libs/jquery.ui", "mods/search"],
     function (validation, ui, search) {
-        var form = $('.search_widget');
-        $('#search_widget_input').autocomplete({
-            source: function (req, res) {
-                $.ajax({
-                    url: form.attr('action'),
-                    dataType: 'json',
-                    data: {
-                        search: req.term,
-                        format: 'json'
-                    },
-                    success: function (data) {
-                        res($.map(data, function (item) {
-                            return {
-                                label: item.label, 
-                                value: item.id,
-                                date: item.date,
-                                url: item.url
-                            };
-                        }));
-                    }
-                });
-            },
-            focus: function (event, ui) {
-                this.value = ui.item.label;
-                return false;
-            },
-            select: function (event, ui) {
-                window.location = ui.item.url;
-                return false;
-            },
-            minLength: 2
+        var widget = search.init_widget();
         /*jslint nomen: false*/
-        }).data('autocomplete')._renderItem = function (ul, item) {
-        /*jslint nomen: false*/
+        widget.data('autocomplete')._renderItem = function (ul, item) {
+        /*jslint nomen: true*/
             return $('<li/>')
                 .data('item.autocomplete', item)
                 .append($('<a/>', {
