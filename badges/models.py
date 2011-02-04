678b242258492ac89dd02ba843512c4fb458732c
Eric Buckley <eric.buckley@gmail.com>
2011-02-04 10:22:05 -0500

    created classes for awarding action badges #623

--- badges/models.py
+++ badges/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from actions.models import Action, UserActionProgress
+
+from brabeion import badges
+from brabeion.base import Badge, BadgeAwarded
+
+class ActionBadge(Badge):
+    events = ['action_completed']
+    multiple = False
+    action = None
+
+    def award(self, **state):
+        if not ActionBadge.action:
+            raise NotImplementedError('%s can not be used as a badge' % self.__class__)
+        user = state['user']
+        if UserActionProgress.objects.filter(user=user, 
+                action=ActionBadge.action, is_completed=True).exists():
+            return BadgeAwarded()
+
+for action in Action.objects.all():
+    name = '%s-action-badge' % action.slug
+    attributes = {'slug': name, 'action': action}
+    action_badge = type(name, (ActionBadge,), attributes)
+    badges.register(action_badge)
+
+def possibly_award_badge(sender, instance, created, **kwargs):
+    if instance.is_completed:
+        badges.possibly_award_badge('action_completed', user=instance.user)
+models.signals.post_save.connect(possibly_award_badge, sender=UserActionProgress)
