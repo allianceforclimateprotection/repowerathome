5538de3179ae2acda6921ac5453a7aede16478f7
Eric Buckley <eric.buckley@gmail.com>
2011-02-25 17:24:37 -0500

    added a new response comment form to the end of a top level comment or the last child response, note this type of comment does not require a message_type, but a message type of "Comment" will automatically be recorded. [completed: 639]

--- rah_comments/forms.py
+++ rah_comments/forms.py
@@ -10,15 +10,18 @@ COMMENT_TYPES = (
 
 class RahCommentForm(ThreadedCommentForm):
     def __init__(self, target_object, parent=None, data=None, initial=None):
+        require_message_type = not bool(parent or (data and data['parent']))
         self.base_fields.insert(
                 self.base_fields.keyOrder.index('comment'), 'message_type',
-                forms.ChoiceField(choices=COMMENT_TYPES, widget=forms.RadioSelect)
+                forms.ChoiceField(choices=COMMENT_TYPES, widget=forms.RadioSelect,
+                    required=require_message_type)
             )
-        self.parent = parent
-        if initial is None:
-            initial = {}
-        initial.update({'parent': self.parent})
-        super(RahCommentForm, self).__init__(target_object, data=data, initial=initial)
+        super(RahCommentForm, self).__init__(target_object, parent=parent, data=data, initial=initial)
+    
+    def clean(self):
+        if self.cleaned_data['parent']:
+            self.cleaned_data['message_type'] = u'C'
+        return super(RahCommentForm, self).clean()
 
     def get_comment_create_data(self):
         d = super(RahCommentForm, self).get_comment_create_data()
